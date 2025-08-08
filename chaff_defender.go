a3407d15e0b105227eeefd29e5d0ae984e6e19dc
justus237 <62303964+justus237@users.noreply.github.com>
2025-08-08 17:46:00 +0200

    stderr instead of stdout

--- chaff_defender.go
+++ chaff_defender.go
@@ -3,6 +3,7 @@ package quic
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"math"
 	"math/rand/v2"
 	"os"
@@ -95,7 +96,7 @@ func samplePacketTimestamps(peakMin, peakMax float64, maxPackets uint32, rng *ra
 	// discretized just means we sample integers instead of floats
 	nofPackets := rng.IntN(int(maxPackets)) + 1
 	weight := ((peakMax - peakMin) * rng.Float64()) + peakMin
-	fmt.Printf("#packets: %d, peak of trace: %f\n", nofPackets, weight)
+	log.Printf("#packets: %d, peak of trace: %f\n", nofPackets, weight)
 	timestamps := make([]time.Duration, nofPackets)
 	for i := 0; i < int(nofPackets); i++ {
 		// sample from rayleigh distribution
@@ -140,7 +141,7 @@ func (def *chaffDefender) Start(now time.Time) {
 		def.start = now
 		def.controlInterval = time.Millisecond * 5
 		def.nextUpdate = now
-		fmt.Printf("starting chaff defender for %s @ Instant %s\n", def.serverName, now)
+		log.Printf("starting chaff defender for %s @ Instant %s\n", def.serverName, now)
 	}
 }
 func (def *chaffDefender) InitTrace(defenseConfig defenseConfig, serverName string) {
@@ -148,29 +149,29 @@ func (def *chaffDefender) InitTrace(defenseConfig defenseConfig, serverName stri
 		def.serverName = serverName
 		//read seed from env var, otherwise randomly generate
 		seedFromEnv, exists := os.LookupEnv("FRONT_SEED")
-		fmt.Println("seedFromEnv:", seedFromEnv)
+		log.Println("seedFromEnv:", seedFromEnv)
 		seed := rand.Uint64()
 		if exists {
 			if seedParsed, err := strconv.ParseUint(seedFromEnv, 10, 64); err == nil {
 				seed = seedParsed
 			} else {
-				fmt.Println("could not parse env seed")
+				log.Println("could not parse env seed")
 			}
 		}
-		fmt.Println("seed:", seed)
+		log.Println("seed:", seed)
 		defenseConfig.SetSeed(seed)
 		def.defenseTrace = defenseConfig.InitTrace()
-		fmt.Printf("generated trace (len %d):\n[", len(def.defenseTrace))
+		log.Printf("generated trace (len %d):\n[", len(def.defenseTrace))
 		for _, traceTime := range def.defenseTrace {
-			fmt.Printf("%d, ", traceTime.Milliseconds())
+			log.Printf("%d, ", traceTime.Milliseconds())
 		}
-		fmt.Printf("]\n")
+		log.Printf("]\n")
 		csvPath, exists := os.LookupEnv("TRACE_CSV_DIR")
 		if exists {
 			path := filepath.Join(csvPath, fmt.Sprintf("%s-front-defense-seed-%s.csv", def.serverName, strconv.FormatUint(seed, 10)))
 			file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
 			if err != nil {
-				fmt.Printf("failed creating file: %s", err)
+				log.Printf("failed creating file: %s", err)
 				return
 			}
 			dataWriter := bufio.NewWriter(file)
@@ -207,26 +208,26 @@ func (def *chaffDefender) ProcessTimer(now time.Time) {
 		fmt.Println("DEFENSE DONE")
 	}
 	if def.start.IsZero() || !def.end.IsZero() {
-		fmt.Println("ProcessTimer called before start or after end")
+		log.Println("ProcessTimer called before start or after end")
 		return
 	}
 	if len(def.defenseTrace) == 0 || def.defenseTrace == nil {
-		fmt.Println("ProcessTimer called before trace init")
+		log.Println("ProcessTimer called before trace init")
 		return
 	}
 	if now.Before(def.nextUpdate) {
-		fmt.Println("ProcessTimer called before next control interval")
+		log.Println("ProcessTimer called before next control interval")
 		return
 	}
-	fmt.Printf("processing timer, ms since start of trace: %d @ Instant %s\n", now.Sub(def.start).Milliseconds(), now)
+	log.Printf("processing timer, ms since start of trace: %d @ Instant %s\n", now.Sub(def.start).Milliseconds(), now)
 	// empty the current action queue from last control interval with a fuzzy sliding window of 3 ms
 	// this ensures that we don't grow the action queue infinitely when packet loss occurs frequently
 	// also the API for time in go is ridiculous
-	fmt.Printf("cleaning action queue, old length: %d", len(def.actionQueue))
+	log.Printf("cleaning action queue, old length: %d", len(def.actionQueue))
 	for len(def.actionQueue) > 0 && def.actionQueue[0].Before(now.Add(-3*time.Millisecond)) {
 		def.actionQueue = def.actionQueue[1:]
 	}
-	fmt.Printf("; new length: %d\n", len(def.actionQueue))
+	log.Printf("; new length: %d\n", len(def.actionQueue))
 
 	//convert real time to trace time (i.e., from time instant to duration since start)
 	endOfCurrentControlInterval := now.Add(def.controlInterval).Sub(def.start)
@@ -235,7 +236,7 @@ func (def *chaffDefender) ProcessTimer(now time.Time) {
 	// add all packets that should be sent in the next 5 ms
 	// effectively this means we will drift by up to 5 ms compared to the original trace
 	// it also does not seem to matter whether we look at the next 5 ms in the trace or the past 5 ms
-	fmt.Printf("[processTimer] defense trace left: %d\n", len(def.defenseTrace))
+	log.Printf("[processTimer] defense trace left: %d\n", len(def.defenseTrace))
 	for len(def.defenseTrace) > 0 && def.defenseTrace[0] < endOfCurrentControlInterval {
 		// definitely not safe from goroutines
 		// convert the durations back to absolute timestamps
@@ -259,5 +260,5 @@ func (def *chaffDefender) SentChaffPacket(now time.Time) {
 	// pop front and compare timestamps
 	nextTimeToSend := def.actionQueue[0]
 	def.actionQueue = def.actionQueue[1:]
-	fmt.Printf("packet [%d] expected at time %s; now is %s; delta %s\n", nextTimeToSend.Sub(def.start).Milliseconds(), nextTimeToSend, now, now.Sub(nextTimeToSend))
+	log.Printf("packet [%d] expected at time %s; now is %s; delta %s\n", nextTimeToSend.Sub(def.start).Milliseconds(), nextTimeToSend, now, now.Sub(nextTimeToSend))
 }
